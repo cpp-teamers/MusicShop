1c0b34e0027d93d3d2e5265317e329d05e3f0606
Student <student@lan.itstep.kiev.ua>
2021-05-12 19:00:19 +0300

    Added ViewModels classes, RelayCommand class

--- MusicShop/Views/MainWindow.xaml.cs
+++ MusicShop/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MusicShop
+namespace MusicShop.Views
 {
     public partial class MainWindow : Window
     {
