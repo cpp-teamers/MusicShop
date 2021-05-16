3bf57cc72e23d7d280b65b7dad7ed367f2c45c42
MrZend <vlad04102002@gmail.com>
2021-05-16 22:10:27 +0300

    added: Discount, Genre, Author, Plate, Publisher, Role, Sale, Track Implementations of these class

--- BizLibrary/Repositories/Implementations/RoleRepository.cs
+++ BizLibrary/Repositories/Implementations/RoleRepository.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BizLibrary.Repositories.Interfaces;
+using ModelsLibrary.Models;
+using ModelsLibrary.EF;
 
 namespace BizLibrary.Repositories.Implementations
 {
-    class RoleRepository
+    class RoleRepository : IRoleRepository
     {
+        private ModelsManager _modelManager = new ModelsManager();
+        public IEnumerable<Role> GetAllRoles()
+        {
+            return _modelManager.Roles.ToList();
+        }
     }
 }
