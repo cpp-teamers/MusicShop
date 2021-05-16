975895518ff878837e63d5edf63153f82781f1df
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-17 00:03:17 +0300

    add: password hashing

--- BizLibrary/ViewModels/AccountViewModel.cs
+++ BizLibrary/ViewModels/AccountViewModel.cs
@@ -8,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Linq;
+using HashGenerators;
 
 namespace MusicShop.ViewModels
 {
@@ -77,7 +78,66 @@ namespace MusicShop.ViewModels
             }
         }
 
-        
+
+        private ICommand _execBack;
+        public ICommand ExecBack
+        {
+            get
+            {
+                return _execBack ?? new RelayCommand(obj =>
+                {
+                    Grid ContainElemsGrid = (obj as Grid);
+
+                    StackPanel initButtons = default;
+                    Border backButton = default;
+
+                    foreach (var item in ContainElemsGrid.Children)
+                    {
+                        if (item is Border potentialBackButton && potentialBackButton.Name == "BackButton")
+                        {
+                            backButton = potentialBackButton;
+                            continue;
+                        }
+
+                        if (item is StackPanel potentialcontElemStack && potentialcontElemStack.Name == "LoginRegButtonPanel")
+                        {
+                            initButtons = potentialcontElemStack;
+                            continue;
+                        }
+
+                        if (item is DockPanel potentialReg && potentialReg.Name == "RegisterPanel" && potentialReg.Visibility == Visibility.Visible)
+                        {
+                            InvisBackButton(backButton);
+                            potentialReg.Visibility = Visibility.Hidden;
+                            continue;
+                        }
+
+                        if (item is DockPanel potentialLog && potentialLog.Name == "LoginPanel" && potentialLog.Visibility == Visibility.Visible)
+                        {
+                            InvisBackButton(backButton);
+                            potentialLog.Visibility = Visibility.Hidden;
+                            break;
+                        }
+                    }
+
+                    initButtons.Visibility = Visibility.Visible;
+                });
+            }
+        }
+        #region 'back' button manipulations
+
+        private void OutvisBackButton(Border borderWithButton)
+        {
+            borderWithButton.Visibility = Visibility.Visible;
+        }
+        private void InvisBackButton(Border borderWithButton)
+        {
+            borderWithButton.Visibility = Visibility.Hidden;
+        }
+        #endregion
+
+        #region Into Registration Logic
+
         private ICommand _execRegistation;
         public ICommand ExecRegistation
         {
@@ -90,7 +150,7 @@ namespace MusicShop.ViewModels
                         Account newAcc = new Account()
                         {
                             Login = ValidateLogin(),
-                            Password = ValidatePassword(),
+                            Password = MD5Generator.ProduceMD5Hash(ValidatePassword()),
                             Email = ValidateEmail(),
                             Phone = ValidatePhone()
                         };
@@ -106,8 +166,57 @@ namespace MusicShop.ViewModels
                 });
 			}
 		}
-
+        #endregion
        
+        #region Validation
+
+        private string ValidateLogin()
+        {
+            if (string.IsNullOrWhiteSpace(_login))
+            {
+                throw new System.Exception("Login Field is Empty!");
+            }
+            if (_login.Length >= 100)
+            {
+                throw new System.Exception("Login is too large!");
+            }
+
+            Regex regExp = new Regex(@"^(?=.*[A-Za-z0-9]$)[A-Za-z\d.-][A-Za-z\d.-]{0,19}");
+            if (!regExp.IsMatch(_login))
+            {
+                throw new System.Exception("Login is invalid! Try again...");
+            }
+            return _login;
+        }
+
+        private string ValidatePassword()
+        {
+            Regex regExp = new Regex(@"^(?=i)(?=.*[a-z])(?=.*[0-9])(?=.*[@#_])[a-z][a-z0-9@#_]{6,}[a-z0-9]$");
+            if (!regExp.IsMatch(_password))
+            {
+                throw new System.Exception("Password is invalid! Try again...");
+            }
+            return _password;
+        }
+
+        private string ValidateEmail()
+        {
+            MailAddress email = new MailAddress(_email);
+            return email.Address;
+        }
+
+        private string ValidatePhone()
+        {
+            Regex regExp = new Regex(@"^([\+]?33[-]?|[0])?[1-9][0-9]{8}$");
+            if (!regExp.IsMatch(_phone))
+            {
+                throw new System.Exception("Phone is invalid! Try again...");
+            }
+            return _phone;
+        }
+        #endregion
+
+        #region init buttons logic
         private ICommand _turnOnLogin;
         public ICommand TurnOnLogin
         {
@@ -116,36 +225,35 @@ namespace MusicShop.ViewModels
                 return _turnOnLogin ?? new RelayCommand(obj =>
                 {
                     Grid ContainElemsGrid = (obj as Grid);
-                    
-                    Border backButton = default; 
+
+                    Border backButton = default;
                     foreach (var item in ContainElemsGrid.Children)
-					{
+                    {
 
                         if (item is Border potentialBackButton && potentialBackButton.Name == "BackButton")
-						{
+                        {
                             backButton = potentialBackButton;
                             continue;
                         }
 
-                        if (item is StackPanel contElemStack && contElemStack.Name == "LoginRegButtonPanel") 
+                        if (item is StackPanel contElemStack && contElemStack.Name == "LoginRegButtonPanel")
                         {
                             contElemStack.Visibility = Visibility.Hidden;
                             continue;
-                        } 
+                        }
 
-						if (item is DockPanel LoginP && LoginP.Name == "LoginPanel")
+                        if (item is DockPanel LoginP && LoginP.Name == "LoginPanel")
                         {
                             OutvisBackButton(backButton);
                             LoginP.Visibility = Visibility.Visible;
                             break;
                         }
-					} 
-                    
+                    }
+
                 });
             }
         }
 
-       
         private ICommand _turnOnReg;
         public ICommand TurnOnReg
         {
@@ -181,111 +289,8 @@ namespace MusicShop.ViewModels
                 });
             }
         }
-
-        private ICommand _execBack;
-        public ICommand ExecBack
-		{
-			get
-			{
-                return _execBack ?? new RelayCommand(obj =>
-                {
-                    Grid ContainElemsGrid = (obj as Grid);
-
-                    StackPanel initButtons = default;
-                    Border backButton = default;
-                    
-                    foreach (var item in ContainElemsGrid.Children)
-                    {
-                        if (item is Border potentialBackButton && potentialBackButton.Name == "BackButton")
-                        {
-                            backButton = potentialBackButton;
-                            continue;
-                        }
-
-                        if (item is StackPanel potentialcontElemStack && potentialcontElemStack.Name == "LoginRegButtonPanel")
-                        {
-                            initButtons = potentialcontElemStack;
-                            continue;
-                        }
-
-                        if (item is DockPanel potentialReg && potentialReg.Name == "RegisterPanel" && potentialReg.Visibility == Visibility.Visible)
-                        {
-                            InvisBackButton(backButton);
-                            potentialReg.Visibility = Visibility.Hidden;
-                            continue;
-                        }
-
-                        if (item is DockPanel potentialLog && potentialLog.Name == "LoginPanel" && potentialLog.Visibility == Visibility.Visible)
-                        {
-                            InvisBackButton(backButton);
-                            potentialLog.Visibility = Visibility.Hidden;
-                            break;
-                        }
-                    }
-
-                    initButtons.Visibility = Visibility.Visible;
-                });
-			}
-		}
-
-		#region 'back' button manipulations
-
-		private void OutvisBackButton(Border borderWithButton)
-		{
-            borderWithButton.Visibility = Visibility.Visible;
-		}
-        private void InvisBackButton(Border borderWithButton)
-        {
-            borderWithButton.Visibility = Visibility.Hidden;
-        }
         #endregion
-        #region Validation
-
-        private string ValidateLogin()
-		{
-			if (string.IsNullOrWhiteSpace(_login))
-			{
-                throw new System.Exception("Login Field is Empty!");
-            }
-			if (_login.Length >= 100)
-			{
-                throw new System.Exception("Login is too large!");
-			}
-
-            Regex regExp = new Regex(@"^(?=.*[A-Za-z0-9]$)[A-Za-z\d.-][A-Za-z\d.-]{0,19}");
-			if (!regExp.IsMatch(_login))
-			{
-                throw new System.Exception("Login is invalid! Try again...");
-            }
-            return _login;
-        }
-
-        private string ValidatePassword()
-        {
-            Regex regExp = new Regex(@"^(?=i)(?=.*[a-z])(?=.*[0-9])(?=.*[@#_])[a-z][a-z0-9@#_]{6,}[a-z0-9]$");
-            if (!regExp.IsMatch(_password))
-            {
-                throw new System.Exception("Password is invalid! Try again...");
-            }
-            return _password;
-        }
-
-        private string ValidateEmail()
-        {
-            MailAddress email = new MailAddress(_email);
-            return email.Address;
-        }
-
-        private string ValidatePhone()
-        {
-            Regex regExp = new Regex(@"^([\+]?33[-]?|[0])?[1-9][0-9]{8}$");
-            if (!regExp.IsMatch(_phone))
-            {
-                throw new System.Exception("Phone is invalid! Try again...");
-            }
-            return _phone;
-        }
-        #endregion
+        
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
