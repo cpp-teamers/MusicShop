8eaefec21f136a4db4b4aca8a65f3719cc77721d
KhomenkoMac <m.khomenko03@gmail.com>
2021-05-16 16:30:54 +0300

    add: water marks& button accept prototype

--- BizLibrary/ViewModels/AccountViewModel.cs
+++ BizLibrary/ViewModels/AccountViewModel.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using ModelsLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MusicShop.ViewModels
 {
     public class AccountViewModel : INotifyPropertyChanged
     {
+        private Account _defaultAcc = new Account()
+        {
+            Login = "Login",
+            Password = "Password",
+            Email = "Email",
+            Phone = "Phone",
+        };
+        public Account DefaultAcc 
+        {
+			get
+			{
+                return _defaultAcc;
+            }
+            set
+			{
+                _defaultAcc = value;
+                OnPropertyChanged(nameof(DefaultAcc));
+			}
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string prop = "")
