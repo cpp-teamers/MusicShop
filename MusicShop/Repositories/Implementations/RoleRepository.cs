6f54f48de93462a3ff4404f26d37ec9772230677
Саня FPV <andrei.cazanov2017@gmail.com>
2021-05-18 11:52:57 +0300

    del: BizLibrary, project fully midified

--- MusicShop/Repositories/Implementations/RoleRepository.cs
+++ MusicShop/Repositories/Implementations/RoleRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BizLibrary.Repositories.Interfaces;
+using MusicShop.Repositories.Interfaces;
 using ModelsLibrary.Models;
 using ModelsLibrary.EF;
 
